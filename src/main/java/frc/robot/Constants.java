a65a6eff635ddcfd41b5dd8ca7593967721eb820
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-01-09 17:05:48 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import frc.lib.util.COTSTalonFXSwerveConstants;
+
+public class Constants {
+    public static final String canivoreName = "canivore";
+    public static final String pigeonCanName = "canivore"; //Pigeon is on separate can loop than others
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.IntakeCommands;
+import frc.robot.commands.PodiumShot;
+import frc.robot.commands.ShooterCommand;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+public class RobotContainer {
+    //Declares/Creates new intake object
+    public static Intake intake  = new Intake();
+    public static Shooter shooter = new Shooter();
+    //Declares the controller
+    private final CommandXboxController controller = new CommandXboxController(0);
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {
+        controller.rightBumper().whileTrue(new IntakeCommands());
+        controller.leftBumper().whileTrue(new ShooterCommand());
+        //controller.x().whileTrue(new PodiumShot());
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
+}
